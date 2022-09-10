174d4bc5975407b33bfaedea9090235c88f94990
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-09-10 23:15:00 +0300

    done

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -5,22 +5,24 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 
-
+/*
+* Крутим кубик, получаем результат
+* */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton : Button = findViewById(R.id.button)
-        rollButton.setOnClickListener{
-            rollButton()
-        }
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollButton() }
     }
-
-    private fun rollButton(){
+/*
+* Функция генерации случайного числа для кубика
+* */
+    private fun rollButton() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView:TextView = findViewById(R.id.textView)
+        val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
     }
 }
